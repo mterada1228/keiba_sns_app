53adc090f35fd156c6e28a9b5b16fd989f0e86f7
Masaru Terada <mterada1228@gmail.com>
2020-02-10 10:35:54 +0000

    before micropost drasitic change

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user, dependent: :destroy
   belongs_to :race, dependent: :destroy
   has_one :hosemark
+  has_many :kaimes
   accepts_nested_attributes_for :hosemark
 end
