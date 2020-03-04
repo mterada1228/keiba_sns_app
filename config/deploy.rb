6b5eaa58b150cc7447295c2c450631b56df0df28
mterada1228 <mterada1228@gmail.com>
2020-03-04 21:31:36 +0900

    remove space deploy_to path

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :branch, 'master'
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
-set :deploy_to, '/var/www/rails/keiba_sns_app '
+set :deploy_to, '/var/www/rails/keiba_sns_app'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
