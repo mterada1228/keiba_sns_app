e56ffcf853180832d5feda8c61cfadaf65bdfcd5
mterada1228 <mterada1228@gmail.com>
2020-03-05 08:33:35 +0900

    bugfix home controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,8 @@ class StaticPagesController < ApplicationController
   def home
     # 現在の日時以降でもっとも近い開催日を持つMenuを返す
     # 上記がnilならば現在の日時からもっとも近い開催日を持つMenuを返す
-    @menu = Menu.where("today_date >= ?",Time.zone.now).order("today_date ASC").first || 
-            Menu.where("today_date <= ?",Time.zone.now).order("today_date DESC").first
+    @menu = Menu.where("today_date >= ?",Time.zone.now).order("today_date DESC").first || 
+            Menu.where("today_date <= ?",Time.zone.now).order("today_date ASC").first
     redirect_to menu_path(@menu)
   end
 
