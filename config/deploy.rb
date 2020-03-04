71925485fedf55e2fd22a8aec944aa80e15bdc6f
mterada1228 <mterada1228@gmail.com>
2020-03-04 20:44:27 +0900

    fix capistrano ver

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "3.4.0"
+lock "3.12.0"
 
 set :application, "keiba_sns_app"
 set :repo_url, "git@github.com:mterada1228/keiba_sns_app.git"
