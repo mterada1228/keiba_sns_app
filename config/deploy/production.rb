8da81bcddce2f126908d00b00df96c3081d4eaf7
mterada1228 <mterada1228@gmail.com>
2020-03-06 09:13:13 +0900

    change cap user masatu to ec2-user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '18.177.202.110', user: 'masaru', roles: %w{app db web} 
+server '18.177.202.110', user: 'ec2-user', roles: %w{app db web} 
 
 # role-based syntax
 # ==================
