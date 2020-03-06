d3ce4236a5887c98da00578d4bc364fcb932d59a
mterada1228 <mterada1228@gmail.com>
2020-03-06 15:59:24 +0900

    fix menu sort rule

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
     # 現在の日時以降でもっとも近い開催日を持つMenuを返す
     # 上記がnilならば現在の日時からもっとも近い開催日を持つMenuを返す
     @menu = Menu.where("today_date >= ?",Time.zone.now).order("today_date DESC").first || 
-            Menu.where("today_date <= ?",Time.zone.now).order("today_date ASC").first
+            Menu.where("today_date <= ?",Time.zone.now).order("today_date DESC").first
     redirect_to menu_path(@menu)
   end
 
