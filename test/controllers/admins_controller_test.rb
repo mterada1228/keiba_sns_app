17d810a70bb88f9500186082cb7c7e9a80f0670e
mterada1228 <mterada1228@gmail.com>
2020-03-27 08:30:55 +0900

    bundle update fog-vsphere

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AdminsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @micropost = assign(:micropost_hitting)
+  end
+
+  test '買い目の的中ステータス更新のテスト' do
+    get update_hitting_admins
+    # 単勝 hit
+
+  end
+
 end
